c5eba6c98d5886ecbca22f68fd2cba77a85fa293
Lloyd Lagura <101856046+lloydlagura123@users.noreply.github.com>
2022-10-30 12:50:38 +0800

    Create round_robin.c

---
+++ round_robin.c
@@ -0,0 +1,74 @@
+int write = 500;
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(write); 
+  digitalWrite(13, LOW);
+  delay(write);
+  
+  digitalWrite(12, HIGH);
+  delay(write);
+  digitalWrite(12, LOW);
+  delay(write);
+  
+  digitalWrite(8, HIGH);
+  delay(write);
+  digitalWrite(8, LOW);
+  delay(write);
+  
+  digitalWrite(7, HIGH);
+  delay(write);
+  digitalWrite(7, LOW);
+  delay(write);
+  
+  digitalWrite(4, HIGH);
+  delay(write);
+  digitalWrite(4, LOW);
+  delay(write);
+  
+  digitalWrite(2, HIGH);
+  delay(write);
+  digitalWrite(2, LOW);
+  delay(write);
+  
+  digitalWrite(2, HIGH);
+  delay(write);
+  digitalWrite(2, LOW);
+  delay(write);
+  
+  digitalWrite(4, HIGH);
+  delay(write);
+  digitalWrite(4, LOW);
+  delay(write);
+  
+  digitalWrite(7, HIGH);
+  delay(write);
+  digitalWrite(7, LOW);
+  delay(write);
+  
+   digitalWrite(8, HIGH);
+  delay(write);
+  digitalWrite(8, LOW);
+  delay(write);
+  
+   digitalWrite(12, HIGH);
+  delay(write);
+  digitalWrite(12, LOW);
+  delay(write);
+  
+    digitalWrite(13, HIGH);
+  delay(write); 
+  digitalWrite(13, LOW);
+  delay(write);
+  
+}
